ba75eb7147911f983c20d52839c77d6d2fd86e98
Jai <jai.nwlj@gmail.com>
2014-02-14 23:28:51 +0000

    Modified Tutorial class to output to Xposed log

--- ChangeClockColour/src/main/java/com/jjgold/changeclockcolour/Tutorial.java
+++ ChangeClockColour/src/main/java/com/jjgold/changeclockcolour/Tutorial.java
@@ -3,5 +3,12 @@ package com.jjgold.changeclockcolour;
 /**
  * Created by Jai on 14/02/14.
  */
-public class Tutorial {
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class Tutorial implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
 }
